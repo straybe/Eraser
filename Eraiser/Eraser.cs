ad67a7657cb3259d252a6e1f12edc7fd9c0f5a53
straybe <enrique66672@gmail.com>
2024-06-14 01:52:20 -0600

    fix(rename): Modify file names

--- Eraiser/Eraser.cs
+++
@@ -1,9 +0,0 @@
-﻿using FluentScheduler;
-
-public class Eraser : Registry
-{
-    public Eraser()
-    {
-        Schedule(() => Console.WriteLine("Han pasado 10 segundos")).ToRunNow().AndEvery(10).Seconds();
-    }
-}
---
+++ Eraser/Program.cs
@@ -0,0 +1,14 @@
+﻿using FluentScheduler;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Inicialización del JobManager con el registro creado
+        JobManager.Initialize(new Eraser());
+
+        // Esto es necesario para que la aplicación no termine inmediatamente
+        Console.WriteLine("Presiona cualquier tecla para salir...");
+        Console.ReadKey();
+    }
+}
