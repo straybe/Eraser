65e06a799966ff4b464c0f53710fe7c52a228bd6
straybe <enrique66672@gmail.com>
2024-06-14 01:47:17 -0600

    Add new file with class Eraser

---
+++ Eraiser/Eraser.cs
@@ -0,0 +1,9 @@
+﻿using FluentScheduler;
+
+public class Eraser : Registry
+{
+    public Eraser()
+    {
+        Schedule(() => Console.WriteLine("Han pasado 10 segundos")).ToRunNow().AndEvery(10).Seconds();
+    }
+}
